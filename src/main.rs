698624fe4af45a921bce482b7bba9fa7dde8dcd2
Evgeni Nabokov <nabokov-ef@yandex.ru>
2019-08-11 01:41:48 -0700

    Added two more katas.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::cmp::Ordering::*;
 use std::collections::HashMap;
+use regex::Regex;
 
 fn main() {
+    let re = Regex::new(r"(\d+)\s(.+?)\s(\w{2}\s\d{5})").unwrap();
+    let text = "123 Main Street St. Louisville OH 43071, 432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432";
+    //let caps = re.captures(text).unwrap();
+    for caps in re.captures_iter(text) {
+        println!("{:?}", caps);
+    }
 }
 
 // https://www.codewars.com/kata/52761ee4cffbc69732000738
@@ -120,4 +127,167 @@ fn comp_tests() {
     let a1 = vec![121, 144, 19, 161, 19, 144, 19, 11];
     let a2 = vec![11*21, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
     comp_test(a1, a2, false);
+}
+
+fn generate_primes(bound: u64) -> Vec<u64> {
+    let mut primes: Vec<bool> = (0..bound + 1).map(|num| num == 2 || num & 1 != 0).collect();
+    let mut num = 3u64;
+    while num * num <= bound {
+        let mut j: u64 = num * num;
+        while j <= bound {
+            primes[j as usize] = false;
+            j += num;
+        }
+        num += 2;
+    }
+    primes.into_iter().enumerate().skip(2).filter_map(|(i, p)| if p {Some(i as u64)} else {None}).collect::<Vec<u64>>()
+}
+
+fn backwards_prime(start: u64, stop: u64) -> Vec<u64> {
+    let mut b_prime_numbers: Vec<u64> = Vec::with_capacity((stop - start / 3) as usize);
+    for n in start..stop + 1 {
+        if is_prime(n) {
+            let rn = reverse(n);
+            if rn != n && is_prime(rn) {
+                b_prime_numbers.push(n);
+            }
+        }
+    }
+    b_prime_numbers
+}
+
+fn is_prime(n: u64)  -> bool {
+    match PRIME_NUMBERS.binary_search(&n) {
+        Ok(_) => return true,
+        Err(i) if i < PRIME_NUMBERS.len() => return false,
+        _ => {
+            for &pn in PRIME_NUMBERS.iter() {
+                if n % pn == 0 { return false; }
+            }
+        }
+    }
+    true
+}
+
+fn reverse(n: u64) -> u64 {
+    let mut nn = n.clone();
+    let mut rn: u64 = 0;
+    while nn > 0 {
+        rn = rn * 10 + nn % 10;
+        nn = nn / 10;
+    }
+    rn
+}
+
+static PRIME_NUMBERS: [u64; 551] = [
+    2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,
+    103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,
+    199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,
+    313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,
+    433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,
+    563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,
+    673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,
+    811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,
+    941,947,953,967,971,977,983,991,997,1009,1013,1019,1021,1031,1033,1039,1049,
+    1051,1061,1063,1069,1087,1091,1093,1097,1103,1109,1117,1123,1129,1151,1153,
+    1163,1171,1181,1187,1193,1201,1213,1217,1223,1229,1231,1237,1249,1259,1277,
+    1279,1283,1289,1291,1297,1301,1303,1307,1319,1321,1327,1361,1367,1373,1381,
+    1399,1409,1423,1427,1429,1433,1439,1447,1451,1453,1459,1471,1481,1483,1487,
+    1489,1493,1499,1511,1523,1531,1543,1549,1553,1559,1567,1571,1579,1583,1597,
+    1601,1607,1609,1613,1619,1621,1627,1637,1657,1663,1667,1669,1693,1697,1699,
+    1709,1721,1723,1733,1741,1747,1753,1759,1777,1783,1787,1789,1801,1811,1823,
+    1831,1847,1861,1867,1871,1873,1877,1879,1889,1901,1907,1913,1931,1933,1949,
+    1951,1973,1979,1987,1993,1997,1999,2003,2011,2017,2027,2029,2039,2053,2063,
+    2069,2081,2083,2087,2089,2099,2111,2113,2129,2131,2137,2141,2143,2153,2161,
+    2179,2203,2207,2213,2221,2237,2239,2243,2251,2267,2269,2273,2281,2287,2293,
+    2297,2309,2311,2333,2339,2341,2347,2351,2357,2371,2377,2381,2383,2389,2393,
+    2399,2411,2417,2423,2437,2441,2447,2459,2467,2473,2477,2503,2521,2531,2539,
+    2543,2549,2551,2557,2579,2591,2593,2609,2617,2621,2633,2647,2657,2659,2663,
+    2671,2677,2683,2687,2689,2693,2699,2707,2711,2713,2719,2729,2731,2741,2749,
+    2753,2767,2777,2789,2791,2797,2801,2803,2819,2833,2837,2843,2851,2857,2861,
+    2879,2887,2897,2903,2909,2917,2927,2939,2953,2957,2963,2969,2971,2999,3001,
+    3011,3019,3023,3037,3041,3049,3061,3067,3079,3083,3089,3109,3119,3121,3137,
+    3163,3167,3169,3181,3187,3191,3203,3209,3217,3221,3229,3251,3253,3257,3259,
+    3271,3299,3301,3307,3313,3319,3323,3329,3331,3343,3347,3359,3361,3371,3373,
+    3389,3391,3407,3413,3433,3449,3457,3461,3463,3467,3469,3491,3499,3511,3517,
+    3527,3529,3533,3539,3541,3547,3557,3559,3571,3581,3583,3593,3607,3613,3617,
+    3623,3631,3637,3643,3659,3671,3673,3677,3691,3697,3701,3709,3719,3727,3733,
+    3739,3761,3767,3769,3779,3793,3797,3803,3821,3823,3833,3847,3851,3853,3863,
+    3877,3881,3889,3907,3911,3917,3919,3923,3929,3931,3943,3947,3967,3989,4001
+];
+
+fn backwards_prime_test(start: u64, stop: u64, exp: Vec<u64>) -> () {
+    assert_eq!(backwards_prime(start, stop), exp)
+}
+
+#[test]
+fn backwards_prime_tests() {
+    let a = vec![13, 17, 31, 37, 71, 73, 79, 97];
+    backwards_prime_test(1, 100, a);
+    let a = vec![13, 17, 31];
+    backwards_prime_test(1, 31, a);
+    let a = vec![701, 709, 733, 739, 743, 751, 761, 769];
+    backwards_prime_test(701, 799, a);
+    let a = vec![1095047, 1095209, 1095319, 1095403];
+    backwards_prime_test(1095000, 1095403, a);
+    let a = vec![700000031];
+    backwards_prime_test(700000008, 700000050, a);
+}
+
+// https://www.codewars.com/kata/56af1a20509ce5b9b000001e
+struct Address<'a> {
+    house_number: &'a str,
+    street_and_town: &'a str,
+    zipcode: &'a str,
+}
+
+fn travel(text: &str, zipcode: &str) -> String {
+    let re = Regex::new(r"(\d+)\s(.+?)\s(\w{2}\s\d{5})").unwrap();
+    let parts = re.captures_iter(text)
+        .map(|caps| Address {
+            house_number: caps.get(1).unwrap().as_str(),
+            street_and_town: caps.get(2).unwrap().as_str(),
+            zipcode: caps.get(3).unwrap().as_str()
+        })
+        .filter(|a| a.zipcode == zipcode)
+        .fold((String::new(), String::new()), |mut parts, address| {
+            if parts.0.len() > 0 {
+                parts.0.push(',');
+                parts.1.push(',');
+            }
+            parts.0.push_str(address.street_and_town);
+            parts.1.push_str(address.house_number);
+            parts
+        });
+    format!("{}:{}/{}", zipcode, parts.0, parts.1)
+}
+
+fn travel_test(r: &str, zipcode:&str, exp: &str) -> () {
+    //println!("r:{:?}", r);
+    println!(" zipcode:{:?}", zipcode);
+    let ans = travel(r, zipcode);
+    println!("actual: {:?}", ans);
+    println!("expect: {:?}", exp);
+    println!("{}", ans == exp);
+    assert_eq!(ans, exp);
+    println!("{}", "-");
+}
+
+#[test]
+fn travel_tests() {
+    let r = &String::from("123 Main Street St. Louisville OH 43071, 432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432,
+      54 Holy Grail Street Niagara Town ZP 32908, 3200 Main Rd. Bern AE 56210,1 Gordon St. Atlanta RE 13000,
+      10 Pussy Cat Rd. Chicago EX 34342, 10 Gordon St. Atlanta RE 13000, 58 Gordon Road Atlanta RE 13000,
+      22 Tokyo Av. Tedmondville SW 43098, 674 Paris bd. Abbeville AA 45521, 10 Surta Alley Goodtown GG 30654,
+      45 Holy Grail Al. Niagara Town ZP 32908, 320 Main Al. Bern AE 56210, 14 Gordon Park Atlanta RE 13000,
+      100 Pussy Cat Rd. Chicago EX 34342, 2 Gordon St. Atlanta RE 13000, 5 Gordon Road Atlanta RE 13000,
+      2200 Tokyo Av. Tedmondville SW 43098, 67 Paris St. Abbeville AA 45521, 11 Surta Avenue Goodtown GG 30654,
+      45 Holy Grail Al. Niagara Town ZP 32918, 320 Main Al. Bern AE 56215, 14 Gordon Park Atlanta RE 13200,
+      100 Pussy Cat Rd. Chicago EX 34345, 2 Gordon St. Atlanta RE 13222, 5 Gordon Road Atlanta RE 13001,
+      2200 Tokyo Av. Tedmondville SW 43198, 67 Paris St. Abbeville AA 45522, 11 Surta Avenue Goodville GG 30655,
+      2222 Tokyo Av. Tedmondville SW 43198, 670 Paris St. Abbeville AA 45522, 114 Surta Avenue Goodville GG 30655,
+      2 Holy Grail Street Niagara Town ZP 32908, 3 Main Rd. Bern AE 56210, 77 Gordon St. Atlanta RE 13000,
+      100 Pussy Cat Rd. Chicago OH 13201");
+    travel_test(r, "AA 45522", "AA 45522:Paris St. Abbeville,Paris St. Abbeville/67,670");
+    travel_test(r, "OH 430", "OH 430:/");
 }
